fe8c9873b5057744a2e6c91325f944069e7b87df
John Nolcox <Jnolcox0429+github@gmail.com>
2017-02-09 20:59:32 -0500

    Wokring on building the interface. Baby steps.

--- react_native_frontend/index.android.js
+++ react_native_frontend/index.android.js
@@ -4,13 +4,47 @@
  * @flow
  */
 
-// THIS IS THE ANDROID ACCESS POINT
-
  import React, { Component } from 'react';
  import {
    AppRegistry,
+   StyleSheet,
+   Text,
+   View
  } from 'react-native';
 
- import Main from './app/main';
+export default class BudgetApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          This is our BudgetApp!
+        </Text>
+        <Text style={styles.inspiration}>
+        This is just the beginning of our 10 week marathon!
+        </Text>
+    );
+  }
+}
 
-AppRegistry.registerComponent('BudgetApp', () => Main);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#424242',
+  },
+  welcome: {
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+    color: #000000,
+  },
+  inspiration: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: #000000,
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('BudgetApp', () => BudgetApp);
