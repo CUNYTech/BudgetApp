f1abf990338ad7a282d16f5486a4ae57ae03a7c5
John Nolcox <Jnolcox0429+github@gmail.com>
2017-02-15 18:05:10 -0500

    Clean-up

--- react_native_frontend/index.ios.js
+++ react_native_frontend/index.ios.js
@@ -1,16 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
- // THIS IS THE IOS ACCESS POINT
-
  import React, { Component } from 'react';
  import {
    AppRegistry,
+   StyleSheet,
+   Text,
+   View
  } from 'react-native';
+ import Splash from './Splash';
+ import Login from './src/components/Login/Login';
 
- import Main from './app/main';
+export default class BudgetApp extends Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
 
-AppRegistry.registerComponent('BudgetApp', () => Main);
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#424242',
+//   },
+//   welcome: {
+//     fontSize: 24,
+//     textAlign: 'center',
+//     margin: 10,
+//     color: #000000,
+//   },
+//   inspiration: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     color: #000000,
+//     marginBottom: 5,
+//   },
+// });
+
+AppRegistry.registerComponent('BudgetApp', () => BudgetApp);
