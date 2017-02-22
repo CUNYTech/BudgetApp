45ff929ce6cd7d01380fb803f5c1e52eb3b59070
nicseve <sevenick@gmail.com>
2017-02-22 11:18:05 -0500

    Update Images.js

--- react_native_frontend/src/theme/Images.js
+++ react_native_frontend/src/theme/Images.js
@@ -2,7 +2,7 @@
 const images = {
   logo: require('../Images/...'),
   clearLogo: require('../Images/...),
-  ignite: require('../Images/...'),
+  any: require('../Images/...'),
   tileBg: require('../Images/...'),
   background: require('../Images/...')
 }
