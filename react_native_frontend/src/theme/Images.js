4a97caaf431ee8e60cd8cb8a4d92ffca221de480
Nicholas Seve <nicseve1@gmail.com>
2017-02-22 11:09:27 -0500

    theme template

---
+++ react_native_frontend/src/theme/Images.js
@@ -0,0 +1,10 @@
+
+const images = {
+  logo: require('../Images/...'),
+  clearLogo: require('../Images/...),
+  ignite: require('../Images/...'),
+  tileBg: require('../Images/...'),
+  background: require('../Images/...')
+}
+
+export default images
