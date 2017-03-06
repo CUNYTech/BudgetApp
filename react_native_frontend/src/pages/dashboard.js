267f5b6e1f9a05ace60cb617e02247e78be82e43
Wellington Vicioso <wellington.vicioso@gmail.com>
2017-03-05 19:09:29 -0500

    add budget tracker bar

--- react_native_frontend/src/pages/dashboard.js
+++ react_native_frontend/src/pages/dashboard.js
@@ -12,7 +12,11 @@ export default class Dashboard extends Component{
 
 
   state = {
-    active: false
+    active: false,
+    budgetTracker: {
+      value: 0,
+      margin: 0
+    }
   };
 
  _signOut(){
@@ -24,6 +28,18 @@ export default class Dashboard extends Component{
   });
  }
 
+ handleBudgetPress() {
+   const num = this.state.budgetTracker.margin
+   const num_v = this.state.budgetTracker.value
+
+   this.setState({
+     budgetTracker: {
+       value: (num_v + 30),
+       margin: (num + 10)
+     }
+   })
+ }
+
  render() {
    const friends = []
    for (var i = 0; i <= 10; i++) {
@@ -33,7 +49,7 @@ export default class Dashboard extends Component{
                      <Text style={{fontSize: 12, fontWeight: 'bold'}}>400 pts</Text>
                    </View>)
                    }
-                   
+    const tracker = this.state.budgetTracker
        return (
            <View style={styles.container}>
              <View style={styles.header}>
@@ -87,10 +103,20 @@ export default class Dashboard extends Component{
               <Text style={{fontFamily: 'OpenSans', fontWeight: '900', fontSize: 20, color: '#1de9b6'}}>
                 Budget
               </Text>
-              <TouchableOpacity style={styles.addExpense} activeOpacity={.7}>
-                <Icon name="plus-circle" size={60} color="#e64a19" style={{}} />
+              <TouchableOpacity
+                onPress={this.handleBudgetPress.bind(this)}
+                style={{ height: 20, marginRight: 20, marginLeft: 20, marginTop: 10, borderWidth: 1, borderColor: '#e0e0e0'}}
+                >
+                <View style={{flex: 1, backgroundColor: '#4527a0', width: this.state.budgetTracker.margin }}></View>
               </TouchableOpacity>
+              <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={{marginLeft: this.state.budgetTracker.margin}}>${this.state.budgetTracker.value}</Text>
+                <Text>$1,000</Text>
+              </View>
             </View>
+            <TouchableOpacity style={styles.addExpense} activeOpacity={.7}>
+              <Icon name="plus-circle" size={60} color="#e64a19" style={{}} />
+            </TouchableOpacity>
            </View>
          )
        }
@@ -122,7 +148,7 @@ export default class Dashboard extends Component{
      },
      addExpense: {
        position: 'absolute',
-       bottom: 30,
-       right: 30,
+       bottom: 10,
+       right: 20,
      }
  });
