270d0f220c31c163a78a81b13d98157669259c56
Miguel Cordero <miguelcordero@dhcp-10-30-39-127.bmcc.cuny.edu>
2017-03-22 14:03:31 -0400

    indent code from merge conflic

--- react_native_frontend/src/pages/index.js
+++ react_native_frontend/src/pages/index.js
@@ -6,18 +6,11 @@ import Friends from './friends';
 import Goals from './goals';
 import Budget from './budget';
 import Points from './points';
-<<<<<<< HEAD
 import Settings from './settings';
 import Profile from './profile';
-=======
->>>>>>> df48ddb0c17b93c6e29b3b5149e1a7866903ee83
 
 
 
 
-<<<<<<< HEAD
 export { Home, Login, Register, Dashboard, Friends, Goals, Budget, Points, Settings, Profile };
 export default { Home, Login, Register, Dashboard, Friends, Goals, Budget, Points, Settings, Profile  };
-=======
-
->>>>>>> df48ddb0c17b93c6e29b3b5149e1a7866903ee83
