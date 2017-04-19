20add37ede293f9106cee3aa03392970bb58c33c
Wellington Vicioso <wellington.vicioso@gmail.com>
2017-04-19 01:45:36 -0400

    remove unused node module

--- react_native_frontend/src/components/snapshots/_dailyPoints.js
+++ react_native_frontend/src/components/snapshots/_dailyPoints.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, Image, Text, StyleSheet, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
-import { StockLine } from 'react-native-pathjs-charts';
 import { Actions } from 'react-native-router-flux';
 import { getPlatformValue } from '../../utils';
 
@@ -11,91 +10,6 @@ const theme = {
   bg: '#212121',
   text: 'white',
   font: 'OpenSans',
-};
-
-const graphData = [
-  [{
-    x: 0,
-    y: 17782,
-  }, {
-    x: 1,
-    y: 28497,
-  }, {
-    x: 2,
-    y: 27128,
-  }, {
-    x: 3,
-    y: 33413,
-  }, {
-    x: 4,
-    y: 48257,
-  }, {
-    x: 5,
-    y: 40579,
-  }, {
-    x: 6,
-    y: 52893,
-  }, {
-    x: 7,
-    y: 60663,
-  }, {
-    x: 8,
-    y: 55715,
-  }, {
-    x: 9,
-    y: 70305,
-  }, {
-    x: 10,
-    y: 88592,
-  }],
-];
-
-
-const options = {
-  width: width * 0.5,
-  height: height * 0.3,
-  margin: {
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  color: theme.accent,
-  gutter: -0.5,
-  animate: {
-    type: 'twoByTwo',
-    duration: 200,
-    fillTransition: 3,
-  },
-  axisX: {
-    showAxis: true,
-    showLines: false,
-    showLabels: true,
-    showTicks: false,
-    zeroAxis: false,
-    orient: 'bottom',
-    label: {
-      marginBottom: 100,
-      fontFamily: 'OpenSans',
-      fontSize: 12,
-      fontWeight: true,
-      fill: 'black',
-    },
-  },
-  axisY: {
-    showAxis: false,
-    showLines: false,
-    showLabels: false,
-    showTicks: false,
-    zeroAxis: false,
-    orient: 'left',
-    label: {
-      fontFamily: 'OpenSans',
-      fontSize: 8,
-      fontWeight: true,
-      fill: 'black',
-    },
-  },
 };
 
 export default class PointsSnapshot extends Component {
